ab1b8a198e8278400af3dc4575b58cd06b6cc9d9
sashankneupane7 <sashankneupane7@gmail.com>
2021-06-18 13:20:13 +0545

    Created Responsive Home Page

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {FaBars} from 'react-icons/fa';
+import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink} from './NavbarElements'
+
+const Navbar = (props) => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">Sashank Neupane</NavLogo>
+            <MobileIcon onClick={props.toggle}>
+              <FaBars />
+            </MobileIcon>
+            <NavMenu>
+              <NavItem>
+                <NavLinks to="blog">Blog</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="projects">Projects</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="about">About</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="contact">Contact</NavLinks>
+              </NavItem>
+            </NavMenu>
+            <NavBtn>
+              <NavBtnLink to="/subscribe">Subscribe</NavBtnLink>
+            </NavBtn>
+        </NavbarContainer>
+      </Nav>
+    </>
+  )
+}             
+
+export default Navbar
---
+++ src/components/navbar/NavbarElements.js
@@ -0,0 +1,126 @@
+import styled from 'styled-components';
+import { Link as LinkR } from 'react-router-dom';
+import { Link as LinkS } from 'react-scroll';
+
+export const Nav = styled.nav`
+  background: black;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  border-bottom: 1px white solid;
+
+  @media screen and (max-width: 960) {
+    transition:0.8s all ease;
+
+  @media screen and (max-width: 500px){
+    max-height: 50px;
+    max-width: 100vw;
+    min-width: 100vw;
+  }
+  }
+`
+
+export const NavbarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 80px;
+  z-index: 1;
+  width: 100%;
+  padding: 0 20px;
+  max-width: 1100px;
+
+  @media screen and (max-width: 500px){
+    max-height: 50px;
+  }
+
+`
+
+export const NavLogo = styled(LinkR)`
+  color: white;
+  justify-self: flex-start;
+  cursor: pointer;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  font-weight: bold;
+  text-decoration: none;
+`
+
+export const MobileIcon = styled.div`
+  display: none;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 60%);
+    font-size: 1.8rem;
+    cursor: pointer;
+    color: white;
+  }
+`
+
+export const NavMenu = styled.ul`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  text-align: center;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`
+
+export const NavItem = styled.li`
+  color: white;
+  margin: 0 10px;
+  padding: 10px 10px;
+`
+
+export const NavLinks = styled(LinkS)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+
+  &.active {
+    border-bottom: 2px solid white;
+  }
+`
+
+export const NavBtn = styled.nav`
+  display: flex;
+  align-items: center;
+
+  @media screen and (max-width: 768px){
+    display: none;
+  }
+`
+
+export const NavBtnLink = styled(LinkR)`
+  border-radius: 20px;
+  background: black;
+  white-space: nowrap;
+  color: white;
+  padding: 10px 20px;
+  font-size: 16px;
+  border: white 1px solid;
+  text-decoration: none;
+  cursor: pointer;
+  transition: all 0.2s ease-ease-in-out;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: white;
+    color: black;
+    padding: 9.6px 19.6px;
+    border: none;
+  }
+`
